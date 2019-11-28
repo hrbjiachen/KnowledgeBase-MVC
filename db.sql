a3e5b4ae1bb4e6a6a6dd34c42e51b81ab25b9943
haoyang324 <haoyang324@gmail.com>
2019-11-27 17:19:25 -0800

    Update db.sql
    Motified reply table
    Added test post

--- db.sql
+++ db.sql
@@ -20,6 +20,12 @@ CREATE TABLE IF NOT EXISTS `user` (
 /* Test user */
 INSERT INTO `user` (`user_id`, `password`, `email`, `fname`, `lname`, `imgurl`, `description`, `country`, `account_type`, `birthday`) VALUES (1, 'password', 'test1@test.com', 'John', 'Doe', 'https://randomuser.me/api/portraits/men/1.jpg', 'Hello, I am John Doe!', 'Canada', 'NORMAL', '1993-11-05');
 
+/* Test post */
+INSERT INTO `post` (`post_id`, `user_id`, `subject`, `detail`, `topic`, `date_created`, `date_updated`) VALUES
+(1, 1, 'Fusce vel metus vel dui mollis ', 'Nulla eu ligula velit. Pellentesque id arcu posuere, tristique tellus at, pellentesque eros. Cras et nunc non nibh viverra sodales. Duis suscipit purus risus, vitae venenatis dolor dignissim quis. ', 'nodejs', '2019-11-19 04:30:22', '2019-11-19 07:38:17'),
+(2, 1, 'Lorem ipsum dolor sit amet', 'Consectetur adipiscing elit. Phasellus sit amet turpis nec nisi lacinia pulvinar. Phasellus lorem orci, pellentesque non semper sed, vestibulum ut neque. Morbi vitae enim efficitur, dictum nisi a, placerat magna. ', 'sql', '2019-11-19 05:49:57', '2019-11-19 07:38:42'),
+(3, 1, 'Sed arcu enim', 'Consequat non dapibus nec, cursus id ex. Curabitur commodo convallis ipsum. Suspendisse vulputate mauris eu pulvinar iaculis. Donec ligula nisl, blandit ac mi quis, porta auctor massa. Integer nec pharetra ex. ', 'sql', '2019-11-19 05:50:10', '2019-11-19 07:38:38');
+
 
 CREATE TABLE IF NOT EXISTS `post` (
   `post_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
@@ -42,7 +48,7 @@ CREATE TABLE IF NOT EXISTS `reply` (
   `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `date_updated` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`reply_id`),
-  KEY `user_id_FK` (`user_id`),
+  KEY `post_id_FK` (`post_id`),
   CONSTRAINT `post_id_FK` FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`) ON DELETE CASCADE ON UPDATE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
